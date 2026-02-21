17126411eaa834de61eaec8f12edc6b7aa959b81
chayn-01 <cda3154@students.uc-bcf.edu.ph>
2026-02-21 01:32:29 +0000

    Added movies array to Product Controller

--- Ayaoan,C_Midterm_Exam_Application/app/Http/Controllers/Controller.php
+++ Ayaoan,C_Midterm_Exam_Application/app/Http/Controllers/Controller.php
@@ -2,7 +2,30 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+
+class ProductController extends Controllers
 {
-    //
+    public function index()
+    {
+        $movies = [
+            [
+                'title' => 'Divergent',
+                'genre' => 'Science Fiction',
+                'price' => 350
+            ],
+            [
+                'title' => 'The Artifice',
+                'genre' => 'Sci-fi, Thriller',
+                'price' => 300
+            ],
+            [
+                'title' => 'Meet, Greet and Goodbye',
+                'genre' => 'Drama, Family',
+                'price' => 400
+            ]
+        ];
+
+        return view('movies', compact('movies'));
+    }
 }
